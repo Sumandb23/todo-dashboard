d027c739cd5ec0f63e7154f18d9889bf396193cb
Sumandb23 <sumancyber22@gmail.com>
2026-08-15 22:41:00 +0530

    Connect frontend to my backend

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
 
         try {
             const response = await axios.post(
-                "https://todo-backend-ocq4.onrender.com/api/auth/login",
+            "https://todo-dashboard-wm5a.onrender.com/api/auth/login",
                 {
                     email,
                     password
--- Frontend/src/components/Todo.jsx
+++ Frontend/src/components/Todo.jsx
@@ -15,7 +15,7 @@ function Todo() {
 
     const token = localStorage.getItem("token");
 
-    const API_URL = "https://todo-backend-ocq4.onrender.com/api/todos";
+    const API_URL = "https://todo-dashboard-wm5a.onrender.com/api/todos";
 
     const getTodo = async () => {
         try {
